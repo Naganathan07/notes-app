ac02b177d3bcf97f587d1a760a0b13ae1fb69e6e
Naganathan07 <111693214+Naganathan07@users.noreply.github.com>
2024-04-16 12:25:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const notesContainer = document.querySelector(".notes-container");
+const btn =decument.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function fun1(){
+    
+}
